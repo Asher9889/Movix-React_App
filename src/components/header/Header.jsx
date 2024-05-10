15f51e9a47df410de29142f04b93a958b1f30192
Asher9889 <saurabhkushwaha9889@gmail.com>
2024-05-11 01:13:32 +0530

    home navigation complete

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -5,65 +5,94 @@ import ContentWrapper from "../contentWrapper/ContentWrapper";
 import { IoSearchSharp } from "react-icons/io5";
 import { IoMenu } from "react-icons/io5";
 import { IoCloseOutline } from "react-icons/io5";
-
-
+import { useNavigate } from "react-router-dom";
 
 const Header = () => {
+  const [mobileMenu, setMobileMenu] = useState(false);
+  const [showMobileMenu, setShowMobileMenu] = useState(false);
+  const [query, setQuery] = useState(null);
+  const [showSearch, setShowSearch] = useState(false)
+  const navigate = useNavigate();
 
-  const[mobileMenu, setMobileMenu] = useState(false)
-  const [showMobileMenu, setShowMobileMenu] = useState(false)
+  const showMenu = () => {
+    setMobileMenu(true);
+    setShowMobileMenu(true);
+    setShowSearch(false)
+  };
+  const hideMenu = () => {
+    setMobileMenu(false);
+    setShowMobileMenu(false);
+  };
 
-  const showMenu = ()=>{
-    setMobileMenu(true)
-    setShowMobileMenu(true)
-  }
-  const hideMenu = ()=>{
+  const menuNavigationHandle = (type)=>{
     setMobileMenu(false)
-    setShowMobileMenu(false)
+    if(type === "movie") {navigate("/explore/movie")}
+    else if(type === "tv") {navigate("/explore/tv")}
   }
-  return (<>
-    <header className={`header ${showMobileMenu ? "backgroundF" : ""} `}>
-      <ContentWrapper >
-        <div>
-          <img src={MovixLogo} alt="" />
-        </div>
 
-        <ul className="desktopMenu">
-          <li>Movie</li>
-          <li>TV Shows</li>
-          <li>
-            <IoSearchSharp />
-          </li>
-        </ul>
+  const searchInputHandle = (e) => {
+    if (e.key === "Enter" && query.length > 0) {
+      navigate(`/search/${query}`);
+      setTimeout(()=>{
+        setShowSearch(false)
+      },100)
+    }
+  };
 
-       
+  return (
+    <>
+      <header className={`header ${showMobileMenu ? "backgroundF" : ""} `}>
+        <ContentWrapper>
+          <div>
+            <img onClick={()=>navigate("/")} src={MovixLogo} alt="" />
+          </div>
 
-        <div className="mobileMenu">
-          <IoSearchSharp />
-          {!mobileMenu ? (<IoMenu onClick={showMenu} />) : (<IoCloseOutline onClick={hideMenu} />)}
-          
-        </div>
-        
-
-
-       
-      </ContentWrapper>
-
-      {mobileMenu ? <div className="showMobileMenu">
-          <ul>
-            <li className="mobileMenuItems">
-              Movies
-            </li>
-            <hr />
-            <li className="mobileMenuItems">
-              TV Shows
+          <ul className="desktopMenu">
+            <li onClick={()=>menuNavigationHandle("movie")} >Movie</li>
+            <li onClick={()=>menuNavigationHandle("tv")}>TV Shows</li>
+            <li>
+              <IoSearchSharp className="searchDesktop" onClick={()=>{setShowSearch(true)
+              setMobileMenu(false)
+            }} />
             </li>
           </ul>
-        </div> : null}
 
-    </header>
-    
-    </>);
+          <div className="mobileMenu">
+            <IoSearchSharp onClick={()=>{setShowSearch(true)
+              setMobileMenu(false)
+            }} />
+            {!mobileMenu ? (
+              <IoMenu onClick={showMenu} />
+            ) : (
+              <IoCloseOutline onClick={hideMenu} />
+            )}
+          </div>
+        </ContentWrapper>
+
+        {mobileMenu ? (
+          <div className="showMobileMenu">
+            <ul>
+              <li className="mobileMenuItems" onClick={()=>menuNavigationHandle("movie")}>Movies</li>
+              <hr />
+              <li className="mobileMenuItems" onClick={()=>menuNavigationHandle("tv")}>TV Shows</li>
+            </ul>
+          </div>
+        ) : null}
+
+       {showSearch ?  <div className="searchInput">
+          <ContentWrapper>
+            <input
+              onKeyUp={searchInputHandle}
+              onChange={(e) => setQuery(e.target.value)}
+              type="text"
+              placeholder="search for movie and tv shows..."
+            />
+            <IoCloseOutline  onClick={()=>setShowSearch(false)} className="closeIcon"  />
+          </ContentWrapper>
+        </div> : null}
+      </header>
+    </>
+  );
 };
 
 export default Header;
