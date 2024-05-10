74fcfcd2294782eb5f112d13200322289d170eeb
Asher9889 <saurabhkushwaha9889@gmail.com>
2024-05-10 18:07:23 +0530

    header mobile func complete

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,17 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style.scss";
-import MovixIcon from "/movix-logo.png";
 import MovixLogo from "../../assets/movix-logo.svg";
 import ContentWrapper from "../contentWrapper/ContentWrapper";
+import { IoSearchSharp } from "react-icons/io5";
+import { IoMenu } from "react-icons/io5";
+import { IoCloseOutline } from "react-icons/io5";
+
+
 
 const Header = () => {
-  return (
-    <header>
-      <ContentWrapper>
-        <div className=".header"></div>
+
+  const[mobileMenu, setMobileMenu] = useState(false)
+  const [showMobileMenu, setShowMobileMenu] = useState(false)
+
+  const showMenu = ()=>{
+    setMobileMenu(true)
+    setShowMobileMenu(true)
+  }
+  const hideMenu = ()=>{
+    setMobileMenu(false)
+    setShowMobileMenu(false)
+  }
+  return (<>
+    <header className={`header ${showMobileMenu ? "background" : ""} `}>
+      <ContentWrapper >
+        <div>
+          <img src={MovixLogo} alt="" />
+        </div>
+
+        <ul className="desktopMenu">
+          <li>Movie</li>
+          <li>TV Shows</li>
+          <li>
+            <IoSearchSharp />
+          </li>
+        </ul>
+
+       
+
+        <div className="mobileMenu">
+          <IoSearchSharp />
+          {!mobileMenu ? (<IoMenu onClick={showMenu} />) : (<IoCloseOutline onClick={hideMenu} />)}
+          
+        </div>
+        
+
+
+       
       </ContentWrapper>
+
+      {mobileMenu ? <div className="showMobileMenu">
+          <ul>
+            <li className="mobileMenuItems">
+              Movies
+            </li>
+            <hr />
+            <li className="mobileMenuItems">
+              TV Shows
+            </li>
+          </ul>
+        </div> : null}
+
     </header>
-  );
+    
+    </>);
 };
 
 export default Header;
